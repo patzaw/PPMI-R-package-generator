9c5d2ea7a6d89a25aa702175838339e6a9385adb
Patrice Godard <patrice.godard@ucb.com>
2020-09-17 17:35:35 +0200

    Keep subjects with an enrolled or a complete status by default

--- Package-Template/R/getCohort.R
+++ Package-Template/R/getCohort.R
@@ -1,17 +1,18 @@
 #' Get patient IDs of a cohort
 #' 
 #' @param cohort the name of the cohort to extract
-#' @param enrolledOnly if TRUE (default) only patients with an "Enrolled"
-#' status are returned
+#' @param enrolled_or_complete_only if TRUE (default) only patients
+#' with an "Enrolled" or a "Complete" status are returned
 #' 
 #' @export
 #' 
-getCohort <- function(cohort, enrolledOnly=TRUE){
-    if(enrolledOnly){
+getCohort <- function(cohort, enrolled_or_complete_only=TRUE){
+    if(enrolled_or_complete_only){
         return(
             rownames(PPMI::patientData)[which(
                 PPMI::patientData$ENROLL_CAT %in% cohort &
-                    PPMI::patientData$ENROLL_STATUS=="Enrolled"
+                    (PPMI::patientData$ENROLL_STATUS %in%
+                    c("Enrolled", "Complete"))
             )]
         )
     }else{
